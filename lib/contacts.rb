dc8c0edbadd02a261cedf2b8cff710105bb49459
Jose E Reyes <jreyes8631@gmail.com>
2020-03-14 21:50:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,6 @@ require 'pry'
   }
   
 def remove_strawberry(contacts)
-  
-  def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
@@ -26,6 +24,4 @@ def remove_strawberry(contacts)
       end
     end
   end
-end
-
 end
